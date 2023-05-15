4fb5c8fefc5deddc5facb2a5c0fe80597e04d24d
Petar Simonovic <simonovicp12@gmail.com>
2023-05-16 00:29:45 +0200

    added bustrep

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import items from "./Store";
 
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './assets/bootstrap/css/bootstrap.css';
+
+
 import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
